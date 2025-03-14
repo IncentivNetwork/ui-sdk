dd26fa14485203d4fbffbad991878b0a2734c9e5
DrenIncentiv <dren@incentiv.net>
2025-03-14 14:01:52 +0100

    Implements configurable Paymaster API data

--- src/PaymasterAPI.ts
+++ src/PaymasterAPI.ts
@@ -4,6 +4,16 @@ import { UserOperationStruct } from '@account-abstraction/contracts'
  * an API to external a UserOperation with paymaster info
  */
 export class PaymasterAPI {
+  private paymasterAndData: string = '0x'
+
+  /**
+   * Sets the paymaster and data value
+   * @param value the value to use for paymasterAndData
+   */
+  setPaymasterAndData(value: string): void {
+    this.paymasterAndData = value
+  }
+
   /**
    * @param userOp a partially-filled UserOperation (without signature and paymasterAndData
    *  note that the "preVerificationGas" is incomplete: it can't account for the
@@ -11,6 +21,6 @@ export class PaymasterAPI {
    * @returns the value to put into the PaymasterAndData, undefined to leave it empty
    */
   async getPaymasterAndData (userOp: Partial<UserOperationStruct>): Promise<string | undefined> {
-    return '0x'
+    return this.paymasterAndData
   }
 }
